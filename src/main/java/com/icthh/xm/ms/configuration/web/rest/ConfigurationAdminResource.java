a40ba95d37c18698f07669524f5ef5d36ca306f3
Serhii Senko <serhii.senko@jevera.software>
2023-09-28 18:15:21 +0300

    Add check on refreshAll

--- src/main/java/com/icthh/xm/ms/configuration/web/rest/ConfigurationAdminResource.java
+++ src/main/java/com/icthh/xm/ms/configuration/web/rest/ConfigurationAdminResource.java
@@ -24,7 +24,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
