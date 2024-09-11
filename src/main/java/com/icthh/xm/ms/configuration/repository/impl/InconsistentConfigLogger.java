5833459f9b1882bfd9bd56461617b90f16219ba8
pzadorovskyi <pzadorovskyi@itsmartflex.com>
2024-09-11 13:53:02 +0300

    logs update

--- src/main/java/com/icthh/xm/ms/configuration/repository/impl/InconsistentConfigLogger.java
+++ src/main/java/com/icthh/xm/ms/configuration/repository/impl/InconsistentConfigLogger.java
@@ -1,18 +1,17 @@
 package com.icthh.xm.ms.configuration.repository.impl;
 
 import com.icthh.xm.commons.config.domain.Configuration;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import org.yaml.snakeyaml.Yaml;
-
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 @Slf4j
 @Component
@@ -20,7 +19,7 @@ public class InconsistentConfigLogger {
 
     private final Yaml yaml;
     private final AtomicBoolean configUpdateInProgress = new AtomicBoolean(false);
-    private final Map<String, StackTraceElement[]> threads = new ConcurrentHashMap<>();
+    private final Map<String, StackTraceElement[]> lockedByThreads = new ConcurrentHashMap<>();
 
     public InconsistentConfigLogger() {
         this.yaml = new Yaml();
@@ -28,25 +27,43 @@ public class InconsistentConfigLogger {
 
     void lock(String updateTriggerMethod) {
         configUpdateInProgress.set(true);
-        threads.put(Thread.currentThread().getName(), Thread.currentThread().getStackTrace());
+        lockedByThreads.put(Thread.currentThread().getName(), Thread.currentThread().getStackTrace());
         log.info("Config update started by method={}", updateTriggerMethod);
     }
 
     void unlock() {
         configUpdateInProgress.set(false);
-        threads.remove(Thread.currentThread().getName());
+        lockedByThreads.remove(Thread.currentThread().getName());
         log.info("Config update ended");
     }
 
     void logConfigGet(String triggerMethod) {
         if (configUpdateInProgress.get()) {
-            log.warn("Config get during update, possible inconsistency triggerMethod={}, lockedBy={}", triggerMethod, threads);
+            String message = "___threadName=%s threadStacksTrace=%s;";
+            String threadsStacksTraceMessage = lockedByThreads.entrySet()
+                .stream()
+                .map(stringStringEntry -> String.format(message, stringStringEntry.getKey(),
+                        Arrays.stream(stringStringEntry.getValue())
+                            .limit(20)
+                            .map(StackTraceElement::toString)
+                            .collect(Collectors.joining(System.lineSeparator()))
+                    )
+                )
+                .collect(Collectors.joining(System.lineSeparator()));
+
+            log.warn(
+                "Config get during update, possible inconsistency triggerMethod={}, lockedByNumberOfThreads={} {}",
+                triggerMethod,
+                lockedByThreads.size(),
+                threadsStacksTraceMessage
+            );
         }
     }
 
     @SneakyThrows
     void logAdditionalParameters(String name, Map<String, Configuration> configs) {
-        Optional<Map<String, Object>> tenantConfigYamlFile = Optional.ofNullable(configs.get("/config/tenants/XM/tenant-config.yml"))
+        Optional<Map<String, Object>> tenantConfigYamlFile = Optional.ofNullable(
+                configs.get("/config/tenants/XM/tenant-config.yml"))
             .map(Configuration::getContent)
             .map(yaml::load);
         String systemClientSecret = tenantConfigYamlFile
@@ -60,6 +77,8 @@ public class InconsistentConfigLogger {
             .map(stringObjectMap -> stringObjectMap.get("host"))
             .map(Object::toString)
             .orElse(StringUtils.EMPTY);
-        log.info("logAdditionalParameters: name: {} systemClientSecret={} redisHost={} configs.size={}", name, systemClientSecret, redisHost, configs.size());
+
+        log.info("logAdditionalParameters: name: {} systemClientSecret={} redisHost={} configs.size={}", name,
+            systemClientSecret, redisHost, configs.size());
     }
 }
