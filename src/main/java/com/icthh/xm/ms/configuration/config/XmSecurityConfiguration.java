ac2678aa28f101745757a437e9c9076b7fdad82f
evgeniy.murenko <z.murenko@gmail.com>
2025-06-04 11:15:17 +0300

    override applyUrlSecurity set to '/config_map' and '/token_key' permitAll

--- src/main/java/com/icthh/xm/ms/configuration/config/XmSecurityConfiguration.java
+++ src/main/java/com/icthh/xm/ms/configuration/config/XmSecurityConfiguration.java
@@ -1,63 +1,35 @@
 package com.icthh.xm.ms.configuration.config;
 
 import com.icthh.xm.commons.permission.access.XmPermissionEvaluator;
-import com.icthh.xm.commons.permission.constants.RoleConstant;
-import com.icthh.xm.commons.security.jwt.JWTConfigurer;
 import com.icthh.xm.commons.security.jwt.TokenProvider;
 import com.icthh.xm.commons.security.spring.config.SecurityConfiguration;
-import com.icthh.xm.commons.security.spring.config.UnauthorizedEntryPoint;
-import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@RequiredArgsConstructor
-@EnableMethodSecurity(securedEnabled = true)
-public class XmSecurityConfiguration {
+public class XmSecurityConfiguration extends SecurityConfiguration {
 
-    private final TokenProvider tokenProvider;
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http
-            .csrf(AbstractHttpConfigurer::disable)
-            .headers(headers -> headers.
-                frameOptions(HeadersConfigurer.FrameOptionsConfig::deny)
-            )
-            .sessionManagement((session) -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            )
-            .authorizeHttpRequests(auth ->
-                auth
-                    .requestMatchers("/api/private/config_map").permitAll()
-                    .requestMatchers("/api/token_key").permitAll()
-                    .requestMatchers("/api/profile-info").permitAll()
-                    .requestMatchers("/api/config").authenticated()
-                    .requestMatchers("/api/tenants").authenticated()
-                    .requestMatchers("/management/health").permitAll()
-                    .requestMatchers("/management/prometheus/**").permitAll()
-                    .requestMatchers("/management/**").hasAuthority(RoleConstant.SUPER_ADMIN)
-                    .requestMatchers("/swagger-resources/configuration/ui").permitAll()
-            );
-        http.with(securityConfigurerAdapter(), Customizer.withDefaults());
-        http.exceptionHandling(handler -> handler.authenticationEntryPoint(new UnauthorizedEntryPoint()));
-        return http.build();
+    public XmSecurityConfiguration(TokenProvider tokenProvider,
+                                   @Value("${jhipster.security.content-security-policy}")
+                                   String contentSecurityPolicy) {
+        super(tokenProvider, contentSecurityPolicy);
     }
 
-    private JWTConfigurer securityConfigurerAdapter() {
-        return new JWTConfigurer(tokenProvider);
+    @Override
+    @SneakyThrows
+    protected HttpSecurity applyUrlSecurity(HttpSecurity http) {
+        http.authorizeHttpRequests(auth ->
+            auth
+                .requestMatchers("/api/private/config_map").permitAll()
+                .requestMatchers("/api/token_key").permitAll()
+        );
+        return super.applyUrlSecurity(http);
     }
 
     @Bean
