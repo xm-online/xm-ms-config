e7d429e10d9de6ba296e57e4859ce3f3d0e5bb40
ssenko <serhii.senko@jevera.software>
2025-09-26 09:45:11 +0300

    add logs to fetch jwk

--- src/main/java/com/icthh/xm/ms/configuration/service/JwkFetcher.java
+++ src/main/java/com/icthh/xm/ms/configuration/service/JwkFetcher.java
@@ -2,7 +2,6 @@ package com.icthh.xm.ms.configuration.service;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -16,10 +15,11 @@ public class JwkFetcher {
     @SneakyThrows
     public String fetchJwk(String jwksEndpointUri) {
         try {
+            log.info("Fetch jwk from URL {}", jwksEndpointUri);
             URL url = new URL(jwksEndpointUri);
             return IOUtils.toString(url.openStream(), UTF_8);
-        } catch (MalformedURLException ex) {
-            log.error("Invalid jwk URL", ex);
+        } catch (Exception ex) {
+            log.error("Error fetch jwk from URL {}", jwksEndpointUri, ex);
         }
         return "";
     }
