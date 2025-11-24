0c46ef762ea0240680c031e7f8eb3181e414cb61
osiievska23 <valentinaosievskaya@gmail.com>
2025-11-24 09:27:50 +0200

    add service name to swagger server url

--- src/main/java/com/icthh/xm/ms/configuration/config/OpenApiConfiguration.java
+++ src/main/java/com/icthh/xm/ms/configuration/config/OpenApiConfiguration.java
@@ -1,6 +1,10 @@
 package com.icthh.xm.ms.configuration.config;
 
+import io.swagger.v3.oas.models.servers.Server;
+import org.apache.commons.lang3.StringUtils;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +12,8 @@ import org.springframework.context.annotation.Profile;
 import tech.jhipster.config.JHipsterConstants;
 import tech.jhipster.config.JHipsterProperties;
 import tech.jhipster.config.apidoc.customizer.JHipsterOpenApiCustomizer;
+
+import java.util.List;
 
 @Configuration
 @Profile(JHipsterConstants.SPRING_PROFILE_API_DOCS)
@@ -29,5 +35,29 @@ public class OpenApiConfiguration {
             .packagesToScan(API_FIRST_PACKAGE)
             .pathsToMatch(properties.getDefaultIncludePattern())
             .build();
+    }
+
+    /**
+     * Add service name to swagger server url
+     * @param appName   serviceName
+     * @return          OpenApiCustomizer
+     */
+    @Bean
+    public OpenApiCustomizer customServerCustomizer(@Value("${spring.application.name:}") String appName) {
+        return openApi -> {
+            if (StringUtils.isNotBlank(appName)) {
+                List<Server> customizedServiceBasePath = openApi.getServers().stream()
+                    .map(Server::getUrl)
+                    .map(url -> buildOpenApiServer(url, appName))
+                    .toList();
+                openApi.setServers(customizedServiceBasePath);
+            }
+        };
+    }
+
+    private Server buildOpenApiServer(String url, String appName) {
+        return new Server()
+            .url(url.replaceAll("/+$", "") + "/" + appName)
+            .description("Customized service base path");
     }
 }
