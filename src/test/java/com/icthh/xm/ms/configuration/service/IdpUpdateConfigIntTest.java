661b6d7a93037dcc6b2139a5c5c462a38a564b74
Yaroslav <@>
2026-03-04 15:44:40 +0200

    XM1-35569 Fix tests

--- src/test/java/com/icthh/xm/ms/configuration/service/IdpUpdateConfigIntTest.java
+++ src/test/java/com/icthh/xm/ms/configuration/service/IdpUpdateConfigIntTest.java
@@ -1,5 +1,28 @@
 package com.icthh.xm.ms.configuration.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.icthh.xm.commons.config.domain.Configuration;
+import com.icthh.xm.commons.messaging.event.system.SystemEvent;
+import com.icthh.xm.ms.configuration.AbstractSpringBootTest;
+import com.icthh.xm.ms.configuration.repository.impl.IdpConfigRepository;
+import com.icthh.xm.ms.configuration.repository.kafka.ConfigTopicProducer;
+import com.icthh.xm.ms.configuration.repository.kafka.SystemQueueConsumer;
+import lombok.SneakyThrows;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.Test;
+import org.mockito.ArgumentMatcher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.icthh.xm.ms.configuration.repository.kafka.InMemoryConfigQueueConsumer.UPDATE_IN_MEMORY;
 import static com.icthh.xm.ms.configuration.web.rest.TestUtil.loadFile;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -11,26 +34,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.icthh.xm.commons.config.domain.Configuration;
-import com.icthh.xm.commons.messaging.event.system.SystemEvent;
-import com.icthh.xm.ms.configuration.AbstractSpringBootTest;
-import com.icthh.xm.ms.configuration.repository.impl.IdpConfigRepository;
-import com.icthh.xm.ms.configuration.repository.kafka.ConfigTopicProducer;
-import com.icthh.xm.ms.configuration.repository.kafka.SystemQueueConsumer;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-import lombok.SneakyThrows;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.junit.Test;
-import org.mockito.ArgumentMatcher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.test.annotation.DirtiesContext;
 
 public class IdpUpdateConfigIntTest extends AbstractSpringBootTest {
 
@@ -66,7 +69,7 @@ public class IdpUpdateConfigIntTest extends AbstractSpringBootTest {
 
         String publicConfig = loadFile("test-idp-config-public.yml");
         idpConfigRepository.onRefresh(TEST_PATH, publicConfig);
-        assertJwkUpdated(1, this::eventEqNoOldClients);
+        assertJwkUpdated(1, this::eventEqNoOldClients, true);
         configurationService.updateConfiguration(new Configuration(TEST_PATH, publicConfig));
         clearInvocations(configTopicProducer, kafkaTemplate, jwkFetcher);
 
@@ -76,11 +79,13 @@ public class IdpUpdateConfigIntTest extends AbstractSpringBootTest {
         verifyNoMoreInteractions(configTopicProducer, kafkaTemplate, jwkFetcher);
     }
 
-    private void assertJwkUpdated(int systemQueueMessages, ArgumentMatcher<String> eventMatcher) {
+    private void assertJwkUpdated(int systemQueueMessages, ArgumentMatcher<String> eventMatcher, boolean isJwksShouldUpdate) {
         verify(kafkaTemplate, times(systemQueueMessages)).send(eq("system_queue"), argThat(eventMatcher));
-        verify(configTopicProducer, times(1)).notifyConfigurationChanged(any(), eq(JWKS));
         verify(jwkFetcher, times(1)).fetchJwk(eq("https://test1.com/certs"));
         verify(jwkFetcher, times(1)).fetchJwk(eq("https://test2.com/certs"));
+        if (isJwksShouldUpdate) {
+            verify(kafkaTemplate, times(1)).send(eq("in_memory_config_topic"), argThat(this::eventEqInMemory));
+        }
         verifyNoMoreInteractions(configTopicProducer, kafkaTemplate, jwkFetcher);
     }
 
@@ -96,6 +101,7 @@ public class IdpUpdateConfigIntTest extends AbstractSpringBootTest {
 
         idpConfigRepository.onRefresh(TEST_PATH, publicConfig);
         verify(kafkaTemplate, times(1)).send(eq("system_queue"), argThat(this::eventEqNoOldClients));
+        verify(kafkaTemplate, times(1)).send(eq("in_memory_config_topic"), argThat(this::eventEqInMemory));
         verifyNoMoreInteractions(kafkaTemplate);
         clearInvocations(kafkaTemplate);
 
@@ -103,7 +109,7 @@ public class IdpUpdateConfigIntTest extends AbstractSpringBootTest {
         idpConfigRepository.onRefresh(TEST_PATH, publicConfig); // emulate 3 nodes
         idpConfigRepository.onRefresh(TEST_PATH, publicConfig); // emulate 3 nodes
 
-        assertJwkUpdated(2, this::eventEq);
+        assertJwkUpdated(2, this::eventEq, false);
         clearInvocations(configTopicProducer, kafkaTemplate, jwkFetcher);
         Thread.sleep(2100); // wait debounce time
 
@@ -113,7 +119,7 @@ public class IdpUpdateConfigIntTest extends AbstractSpringBootTest {
         idpConfigRepository.onRefresh(TEST_PATH, publicConfig); // emulate 3 nodes
 
         // public config updated 2 times, but jwks updated only 1 time
-        assertJwkUpdated(3, this::eventEq);
+        assertJwkUpdated(3, this::eventEq, true);
     }
 
     private void mockInfrastructure() {
@@ -121,15 +127,10 @@ public class IdpUpdateConfigIntTest extends AbstractSpringBootTest {
             systemQueueConsumer.consumeEvent(new ConsumerRecord<>("system_queue", 0, 0, "test", invocation.getArgument(1)));
             return null;
         });
-        AtomicInteger counter = new AtomicInteger(0);
         when(jwkFetcher.fetchJwk(eq("https://test1.com/certs")))
-            .thenAnswer(it -> JWK_TEMPLATE
-                .replace("{mockValue}", "test1")
-                .replace("{mockValueN}", "" + counter.incrementAndGet()));
+            .thenAnswer(it -> jwkConfigFromTemplate(1));
         when(jwkFetcher.fetchJwk(eq("https://test2.com/certs")))
-            .thenAnswer(it -> JWK_TEMPLATE
-                .replace("{mockValue}", "test2")
-                .replace("{mockValueN}", "" + counter.incrementAndGet()));
+            .thenAnswer(it -> jwkConfigFromTemplate(2));
     }
 
     @SneakyThrows
@@ -145,6 +146,34 @@ public class IdpUpdateConfigIntTest extends AbstractSpringBootTest {
         assertEquals(expected.getEventType(), actual.getEventType());
         assertEquals(expected.getMessageSource(), actual.getMessageSource());
         return true;
+    }
+
+    @SneakyThrows
+    private boolean eventEqInMemory(String s) {
+        SystemEvent actual = mapper.readValue(s, SystemEvent.class);
+        List<Map<String, String>> actualJwks = ((List<Map<String, String>>) actual.getData());
+        actualJwks.sort(Comparator.comparing(map -> map.get("path")));
+        List<Map<String, String>> expectedJwks = getExpectedJwks();
+
+        assertEquals("config", actual.getMessageSource());
+        assertEquals(UPDATE_IN_MEMORY, actual.getEventType());
+        assertEquals(expectedJwks, actualJwks);
+        return true;
+    }
+
+    private List<Map<String, String>> getExpectedJwks() {
+        AtomicInteger counter = new AtomicInteger(0);
+        return JWKS.stream()
+            .map( path -> Map.of(
+                "path", path,
+                "content", jwkConfigFromTemplate(counter.incrementAndGet())
+            )).toList();
+    }
+
+    private String jwkConfigFromTemplate(int number) {
+        return JWK_TEMPLATE
+            .replace("{mockValue}", "test" + number)
+            .replace("{mockValueN}", "" + number);
     }
 
     @SneakyThrows
